6511144e6b22f24726e98f6d8dc72e39e077f618
Ejaaz Khan <ejaaz@frappe.io>
2026-04-16 10:10:09 +0530

    test: match state with window state

--- cypress/integration/column.js
+++ cypress/integration/column.js
@@ -96,43 +96,45 @@ describe('Column', function () {
             expect(actual).to.be.closeTo(expected, 1);
         };
 
-        cy.get('.dt-scrollable').then(($scrollable) => {
-            const scrollable = $scrollable[0];
-            const stickyCheckboxBodyCell = Cypress.$('.dt-cell--0-0')[0];
-            const stickyCheckboxHeaderCell = Cypress.$('.dt-cell--header-0')[0];
-            const stickySerialBodyCell = Cypress.$('.dt-cell--1-0')[0];
-            const stickySerialHeaderCell = Cypress.$('.dt-cell--header-1')[0];
-            const stickyCustomBodyCell = Cypress.$('.dt-cell--4-0')[0];
-            const stickyCustomHeaderCell = Cypress.$('.dt-cell--header-4')[0];
-            const regularBodyCell = Cypress.$('.dt-cell--2-0')[0];
+        cy.window().then((win) => {
+            const stickyColumns = win.datatable.getColumns()
+                .filter(column => column.sticky)
+                .map(column => column.colIndex);
+            const nonStickyColumn = win.datatable.getColumns()
+                .find(column => !column.sticky && column.focusable !== false);
 
-            const initialStickyCheckboxBodyLeft = stickyCheckboxBodyCell.getBoundingClientRect().left;
-            const initialStickyCheckboxHeaderLeft = stickyCheckboxHeaderCell.getBoundingClientRect().left;
-            const initialStickySerialBodyLeft = stickySerialBodyCell.getBoundingClientRect().left;
-            const initialStickySerialHeaderLeft = stickySerialHeaderCell.getBoundingClientRect().left;
-            const initialStickyCustomBodyLeft = stickyCustomBodyCell.getBoundingClientRect().left;
-            const initialStickyCustomHeaderLeft = stickyCustomHeaderCell.getBoundingClientRect().left;
-            const initialRegularBodyLeft = regularBodyCell.getBoundingClientRect().left;
+            cy.get('.dt-scrollable').then(($scrollable) => {
+                const scrollable = $scrollable[0];
+                const stickyBodyCells = stickyColumns
+                    .map(colIndex => Cypress.$(`.dt-cell--${colIndex}-0`)[0]);
+                const stickyHeaderCells = stickyColumns
+                    .map(colIndex => Cypress.$(`.dt-cell--header-${colIndex}`)[0]);
+                const regularBodyCell = Cypress.$(`.dt-cell--${nonStickyColumn.colIndex}-0`)[0];
 
-            scrollable.scrollLeft = 220;
-            scrollable.dispatchEvent(new Event('scroll'));
+                const initialStickyBodyLefts = stickyBodyCells
+                    .map(cell => cell.getBoundingClientRect().left);
+                const initialStickyHeaderLefts = stickyHeaderCells
+                    .map(cell => cell.getBoundingClientRect().left);
+                const initialRegularBodyLeft = regularBodyCell.getBoundingClientRect().left;
 
-            cy.wait(50).then(() => {
-                const nextStickyCheckboxBodyLeft = stickyCheckboxBodyCell.getBoundingClientRect().left;
-                const nextStickyCheckboxHeaderLeft = stickyCheckboxHeaderCell.getBoundingClientRect().left;
-                const nextStickySerialBodyLeft = stickySerialBodyCell.getBoundingClientRect().left;
-                const nextStickySerialHeaderLeft = stickySerialHeaderCell.getBoundingClientRect().left;
-                const nextStickyCustomBodyLeft = stickyCustomBodyCell.getBoundingClientRect().left;
-                const nextStickyCustomHeaderLeft = stickyCustomHeaderCell.getBoundingClientRect().left;
-                const nextRegularBodyLeft = regularBodyCell.getBoundingClientRect().left;
+                scrollable.scrollLeft = 220;
+                scrollable.dispatchEvent(new Event('scroll'));
 
-                expectPinned(nextStickyCheckboxBodyLeft, initialStickyCheckboxBodyLeft);
-                expectPinned(nextStickyCheckboxHeaderLeft, initialStickyCheckboxHeaderLeft);
-                expectPinned(nextStickySerialBodyLeft, initialStickySerialBodyLeft);
-                expectPinned(nextStickySerialHeaderLeft, initialStickySerialHeaderLeft);
-                expectPinned(nextStickyCustomBodyLeft, initialStickyCustomBodyLeft);
-                expectPinned(nextStickyCustomHeaderLeft, initialStickyCustomHeaderLeft);
-                expect(nextRegularBodyLeft).to.be.lessThan(initialRegularBodyLeft);
+                cy.wait(50).then(() => {
+                    const nextStickyBodyLefts = stickyBodyCells
+                        .map(cell => cell.getBoundingClientRect().left);
+                    const nextStickyHeaderLefts = stickyHeaderCells
+                        .map(cell => cell.getBoundingClientRect().left);
+                    const nextRegularBodyLeft = regularBodyCell.getBoundingClientRect().left;
+
+                    nextStickyBodyLefts.forEach((left, index) => {
+                        expectPinned(left, initialStickyBodyLefts[index]);
+                    });
+                    nextStickyHeaderLefts.forEach((left, index) => {
+                        expectPinned(left, initialStickyHeaderLefts[index]);
+                    });
+                    expect(nextRegularBodyLeft).to.be.lessThan(initialRegularBodyLeft);
+                });
             });
         });
     });
